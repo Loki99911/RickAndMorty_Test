9b3385a7a780948b23fa91bdb6ef10538eebbfb6
Loki99911 <nikita_kresik@i.ua>
2023-10-06 16:01:39 +0300

    Update charactersSelectors.ts

--- src/redux/Characters/charactersSelectors.ts
+++ src/redux/Characters/charactersSelectors.ts
@@ -2,11 +2,12 @@ import { RootState } from "../store";
 
 export const allCharacters = (state: RootState) =>
   state.charactersInfo.characters;
-  
+
 export const currentCharacter = (state: RootState) =>
   state.charactersInfo.currentCharacter;
-  
+
 export const isLoadingCharacters = (state: RootState) =>
   state.charactersInfo.isLoading;
-  export const isLoadingCurrentCharacter = (state: RootState) =>
-    state.charactersInfo.isLoading;
+
+export const isLoadingCurrentCharacter = (state: RootState) =>
+  state.charactersInfo.isLoading;
