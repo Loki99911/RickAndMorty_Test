cd0e3bb94f22ad11009b2f302752aabb0861f8ea
Loki99911 <nikita_kresik@i.ua>
2023-10-08 13:01:17 +0300

    Update paginationSelectors.ts

--- src/redux/Pagination/paginationSelectors.ts
+++ src/redux/Pagination/paginationSelectors.ts
@@ -1,4 +1,5 @@
 import { RootState } from "../store";
 
 export const totalCount = (state: RootState) => state.paginationInfo.pagination?.count;
+
 export const totalPages = (state: RootState) => state.paginationInfo.pagination?.pages;
